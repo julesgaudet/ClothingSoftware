ef2a4b2cc8406b54855f43f88d9edbe15b82b4e3
Houssam Moussafir <houssam.moussafir.1@ens.etsmtl.ca>
2024-03-20 13:54:54 -0400

    day3 h

--- website/eko-website/src/app/Article/ApercuArticle1.jsx
+++ website/eko-website/src/app/Article/ApercuArticle1.jsx
@@ -1,8 +1,36 @@
-export default function ApercuArticle1({ nom, photoSrc, prix }) {
-  return (
-    <div className="col-span-1">
-      <img src={photoSrc} alt={nom} className="w-full h-auto" />
-    </div>
+'use client'
+import pictureJSON from "../dataJSON/pictureJSON.json";
+import { useRouter } from 'next/navigation';
+export default function ApercuArticle1({vetement}) {
+
+const router = useRouter();
+  const queryString = window.location.search;
+  const parametresURL = new URLSearchParams(queryString);
+  const id = parametresURL.get("id");
+
+let dataPhoto = {};
+   //obtention d'un tableau des photos de l'article
+
+
+
+    // Utiliser id_route dans la suite de votre code
+    //----------------------------------------------------------------------------------------//
     
-  );
+const photo = pictureJSON.find(item => item.id_article === id);
+// Vérifier si l'article a été trouvé
+if (photo) {
+    // Créer un objet contenant les détails de l'article
+    dataPhoto = {
+        id: photo.id_picture || '',
+        url: photo.url || '',
+        idArticle: photo.id_article || '',
+    };
+  }
+  console.log("url: ",dataPhoto.url );
+ return (
+
+   <div className="col-span-1">
+       <img key={dataPhoto.id} src={dataPhoto.url} alt={vetement.nom} className="w-full h-auto" />
+   </div>
+ );
 }
--- website/eko-website/src/app/Article/page.js
+++ website/eko-website/src/app/Article/page.js
@@ -1,60 +1,93 @@
+'use client'
 import ApercuCouleurs1 from "./ApercuCouleurs1";
 import ApercuArticle1 from "./ApercuArticle1";
-import React, { useState } from 'react';
+import articleJSON from "../dataJSON/articleJSON.json";
+import colorsJSON from "../dataJSON/colorsJSON.json";
+
+import { useRouter } from 'next/navigation';
+
 
 export default function Article() {
+  const router = useRouter();
+  const queryString = window.location.search;
+  const parametresURL = new URLSearchParams(queryString);
+  const id = parametresURL.get("id");
+    // Utiliser id_route dans la suite de votre code
+    //----------------------------------------------------------------------------------------//
+    let dataArticle = {};
+const article = articleJSON.find(item => item.id_article === id);
+// Vérifier si l'article a été trouvé
+if (article) {
+    // Créer un objet contenant les détails de l'article
+    dataArticle = {
+        id: article.id_article || '',
+        nom: article.name || '',
+        description: article.description || '',
+        prix: article.price || '',
+        marque: article.brand || '',
+        date: article.upload_date || '',
+        type: article.type || '',
+    };
+  }
+
+
+  const dataColor = colorsJSON.map((color) => ({
+    id_color:color.id_color,
+    color_code:color.color_code,
+    id:color.id_article,
+  }));
+
+
   
-  
-  const dataArticle = [
-    {
-      id_article: "1",
-      name: "Wave Rugby Sweater",
-      description:
-        "100% French terry cotton Dyed buttons with Dime logo on the front Ribbed finish on the cuffs and waist Embroidered logo on the left chest The model is 167 cm tall and wears size Small",
-      price: "135.00",
-      quantiter:"23",
-      type: "Shirt",
-      brand: "Dime",
-      upload_date: "2024-03-14 21:41:21",
-      id_employee: "1",
-      photo: [{
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      {
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      {
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      {
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      {
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      {
-        nom: "Nom de l'article",
-        photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
-        prix: "19.99",
-      },
-      ]
-    }];
+  // const dataArticle = [
+  //   {
+  //     id_article: "1",
+  //     name: "Wave Rugby Sweater",
+  //     description:
+  //       "100% French terry cotton Dyed buttons with Dime logo on the front Ribbed finish on the cuffs and waist Embroidered logo on the left chest The model is 167 cm tall and wears size Small",
+  //     price: "135.00",
+  //     quantiter:"23",
+  //     type: "Shirt",
+  //     brand: "Dime",
+  //     upload_date: "2024-03-14 21:41:21",
+  //     id_employee: "1",
+  //     photo: [{
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     {
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     {
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     {
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     {
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     {
+  //       nom: "Nom de l'article",
+  //       photoSrc: "https://media.gq.com/photos/56cb52771388833772dbc5ea/master/pass/GettyImages-465384280.jpg",
+  //       prix: "19.99",
+  //     },
+  //     ]
+  //   }];
 
   return (
-  
+    
 
     <>
-   
       <header className="p-4 flex items-center justify-between">
         <div className="flex-grow">
         <div className="flex items-center">
@@ -83,38 +116,26 @@ export default function Article() {
         </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         
-        <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
-          
-            
-              {dataArticle.map((article) => (
-                article.photo.map((photo) => (
-                  <ApercuArticle1
-                    key={article.id_article}
-                    nom={article.name}
-                    photoSrc={photo.photoSrc}
-                    prix={article.price}
-                    
-                  />
-                ))
-              ))}
+        <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">               
+            <ApercuArticle1  vetement= {dataArticle} />
             </div>
           
             <div className="col-span-1 row-span-1 grid grid-cols-1 gap-4">
-            {dataArticle.map((article) => (
-              <div key={article.id_article}className="bg-white p-4 rounded">
+           
+              <div key={dataArticle.id}className="bg-white p-4 rounded">
                   <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
-                  <h2 className="text-xl font-bold">{article.name}</h2>
-                  <h2 className="text-l font-semibold text-blue-800">{article.price}$</h2>
+                  <h2 className="text-xl font-bold">{dataArticle.nom}</h2>
+                  <h2 className="text-l font-semibold text-blue-800">{dataArticle.prix}$</h2>
                   </div>
-                  <p className="font-bold size-10">color:{Couleur.name} </p>
+                  
                   <ApercuCouleurs1/>
                   <p className="font-bold size-10">Size </p>
                   <p className="font-bold size-10">Quantity left: {article.quantiter} </p>
                   
                   
-                </div>
+                </div> 
               
-            ))}
+             
           
 
         </div>
