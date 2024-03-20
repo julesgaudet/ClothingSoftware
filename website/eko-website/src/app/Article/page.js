14bbdfcd0d9d3bffb9b341ff6a387e086c47266d
Yougondie22 <cedric.ruel@bell.net>
2024-03-20 11:33:43 -0400

    ajout logo panier

--- website/eko-website/src/app/Article/page.js
+++ website/eko-website/src/app/Article/page.js
@@ -58,22 +58,23 @@ export default function Article() {
         <div className="flex-grow">
         <div className="flex items-center">
       <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center mr-2">
-        {/* Insérez le lien pour le logo <img src="" className="w-6 h-6" alt="Logo" /> */}
         E
       </div>
-      <h1 className="text-2xl font-bold text-blue-600">EKO Clothing Shop</h1>
+      <h1 className="text-2xl font-bold text-blue-600">EKO</h1>
     </div>
         </div>
         <nav className="flex-grow">
           <ul className="flex justify-center space-x-4">
-             {/*j'ai mit seulement des ? pour l'instant, pour lien vers les autres pages */}
-            <li><a href="?" className="text-gray-600 hover:text-black">Woman</a></li>
-            <li><a href="?" className="text-gray-600 hover:text-black">Men</a></li>
+            <li><a href="/Articles" className="text-gray-600 hover:text-black">Clothing</a></li>
             <li><a href="?" className="text-gray-600 hover:text-black">About</a></li>
           </ul>
         </nav>
         <div className="flex-grow flex justify-end">
-          <a href="?" className="text-gray-600 hover:text-black">My Cart</a>
+        <div className="w-8 h-8 justify-center mr-2">
+                <img src="https://www.svgrepo.com/show/363038/shopping-cart-simple-bold.svg" className="w-6 h-6 space-x-2" alt="Panier" ></img>
+
+      </div>
+          <a href="/Cart" className="text-gray-600 hover:text-black">My Cart</a>
         </div>
       </header>
 
