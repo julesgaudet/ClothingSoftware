bfc3a53803b3ce52f6dd3b4162be308152c798c7
Houssam Moussafir <houssam.moussafir.1@ens.etsmtl.ca>
2024-03-22 13:46:17 -0400

    day5 h

--- website/eko-website/src/app/Article/page.js
+++ website/eko-website/src/app/Article/page.js
@@ -12,43 +12,43 @@ export default function Article() {
   const queryString = window.location.search;
   const parametresURL = new URLSearchParams(queryString);
   const id = parametresURL.get("id");
-    // Utiliser id_route dans la suite de votre code
-    //----------------------------------------------------------------------------------------//
-    let dataArticle = {};
-const article = articleJSON.find(item => item.id_article === id);
-// Vérifier si l'article a été trouvé
-if (article) {
+  // Utiliser id_route dans la suite de votre code
+  //----------------------------------------------------------------------------------------//
+  let dataArticle = {};
+  const article = articleJSON.find(item => item.id_article === id);
+  // Vérifier si l'article a été trouvé
+  if (article) {
     // Créer un objet contenant les détails de l'article
     dataArticle = {
-        id: article.id_article || '',
-        nom: article.name || '',
-        description: article.description || '',
-        prix: article.price || '',
-        marque: article.brand || '',
-        date: article.upload_date || '',
-        type: article.type || '',
+      id: article.id_article || '',
+      nom: article.name || '',
+      description: article.description || '',
+      prix: article.price || '',
+      marque: article.brand || '',
+      date: article.upload_date || '',
+      type: article.type || '',
     };
   }
 
 
   const dataColor = colorsJSON.map((color) => ({
-    id_color:color.id_color,
-    color_code:color.color_code,
-    id:color.id_article,
+    id_color: color.id_color,
+    color_code: color.color_code,
+    id: color.id_article,
   }));
 
   return (
-    
+
 
     <>
       <header className="p-4 flex items-center justify-between">
         <div className="flex-grow">
-        <div className="flex items-center">
-      <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center mr-2">
-        E
-      </div>
-      <h1 className="text-2xl font-bold text-blue-600">EKO</h1>
-    </div>
+          <div className="flex items-center">
+            <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center mr-2">
+              E
+            </div>
+            <h1 className="text-2xl font-bold text-blue-600">EKO</h1>
+          </div>
         </div>
         <nav className="flex-grow">
           <ul className="flex justify-center space-x-4">
@@ -57,40 +57,48 @@ if (article) {
           </ul>
         </nav>
         <div className="flex-grow flex justify-end">
-        <div className="w-8 h-8 justify-center mr-2">
-                <img src="https://www.svgrepo.com/show/363038/shopping-cart-simple-bold.svg" className="w-6 h-6 space-x-2" alt="Panier" ></img>
+          <div className="w-8 h-8 justify-center mr-2">
+            <img src="https://www.svgrepo.com/show/363038/shopping-cart-simple-bold.svg" className="w-6 h-6 space-x-2" alt="Panier" ></img>
 
-      </div>
+          </div>
           <a href="/Cart" className="text-gray-600 hover:text-black">My Cart</a>
         </div>
       </header>
 
-      <div className="col-span-1">
-          
-        </div>
+      <div className="col-span-2">
+
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-        
-        <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">               
-            <ApercuArticle1  vetement= {dataArticle} />
+
+        <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
+          <ApercuArticle1 vetement={dataArticle} />
+        </div>
+
+        <div className="col-span-1 row-span-1 grid grid-cols-1 gap-4">
+          <div key={dataArticle.id} className="bg-white p-4 rounded">
+            <small>{dataArticle.marque}</small>
+            <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
+              <h2 className="text-xl font-bold">{dataArticle.nom}</h2>
+              <h2 className="text-l font-semibold text-blue-800">{dataArticle.prix}$</h2>
             </div>
-          
-            <div className="col-span-1 row-span-1 grid grid-cols-1 gap-4">
-           
-              <div key={dataArticle.id}className="bg-white p-4 rounded">
-                  <div className="row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
-                  <h2 className="text-xl font-bold">{dataArticle.nom}</h2>
-                  <h2 className="text-l font-semibold text-blue-800">{dataArticle.prix}$</h2>
-                  </div>
-                  <ApercuCouleurs1/>
-                  <p className="font-bold size-10">Size </p>
-                  {/* quantiter n'ai pas dans articleJSON */}
-                  {/* <p className="font-bold size-10">Quantity left: {dataArticle.quantiter} </p> */}
-                  
-                  
-                </div> 
-              
-             
-          
+            <p>description: {dataArticle.description}</p>
+            <ApercuCouleurs1 />
+            <p className="font-bold size-10">Size </p>
+            <div className="flex flex-wrap gap-4 items-center">
+              <div color="primary">
+                Add to Cart
+              </div>
+            </div>
+            <p className="font-bold size-10">Sustainability</p>
+            <img src="https://as1.ftcdn.net/v2/jpg/00/49/96/80/1000_F_49968019_k7ViQn2cSWXKzFcprrUyg7D2nPFv7zWh.jpg" alt="Photo écoresponsable" className="w-20 h-20 space-x-2" />
+            {/* quantiter n'ai pas dans articleJSON */}
+            {/* <p className="font-bold size-10">Quantity left: {dataArticle.quantiter} </p> */}
+
+
+          </div>
+
+
+
 
         </div>
       </div>
