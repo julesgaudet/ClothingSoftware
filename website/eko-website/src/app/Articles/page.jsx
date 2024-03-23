46673a71214823095f72bad7ed368348cfa67def
Noah Tremblay <noah.tremblay.1@ens.etsmtl.ca>
2024-03-23 15:38:37 -0400

    noah samedi

--- website/eko-website/src/app/Articles/page.jsx
+++ website/eko-website/src/app/Articles/page.jsx
@@ -1,5 +1,5 @@
 "use client"; // important!!!!
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ApercuArticles from "./ApercuArticles";
 import Filtres from "./Filtres";
@@ -35,15 +35,35 @@ export default function Articles() {
 
   //----------------------------------------------------------------------------------------//
   //obtention d'un tableau des articles
-  const data = articleJSON.map((item) => ({
-    id: item.id_article,
-    nom: item.name,
-    description: item.description,
-    prix: item.price,
-    marque: item.brand,
-    date: item.upload_date,
-    type: item.type,
-  }));
+
+  const [data, setData] = useState([]);
+
+  //----------------------------------------------------------------------------------------//
+  // Effect pour récupérer les données depuis l'API
+  useEffect(() => {
+    fetch("http://localhost/api/article/oldestArticle")
+      .then((response) => {
+        console.log("Response before parsing JSON:", response);
+        return response.json();
+      })
+      .then((articleJSON) => {
+        const formattedData = articleJSON.map((item) => ({
+          id: item.id_article,
+          nom: item.name,
+          description: item.description,
+          prix: item.price,
+          marque: item.brand,
+          date: item.upload_date,
+          type: item.type,
+        }));
+        setData(formattedData);
+        console.log(formattedData);
+        console.log(articleJSON);
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite:", error);
+      });
+  }, []); // Assure que ce code ne s'exécute qu'une seule fois après le premier rendu
 
   //----------------------------------------------------------------------------------------//
   //Catégorie select
@@ -62,7 +82,7 @@ export default function Articles() {
   //----------------------------------------------------------------------------------------//
   return (
     <>
-      <div className="bg-[#F5F5F7] pt-4">
+      <div className="bg-[#F5F5F7] min-h-screen pt-4">
         <div className="mx-20 my-2  md:mx-40">
           <div className="flex flex-col my-10">
             <h1 className="font-black text-5xl text-[#3858D6]">
@@ -78,7 +98,7 @@ export default function Articles() {
             <div className="block max-w-80">
               <Filtres articles={data} />
             </div>
-            <ApercuArticles dataArticles={data} />
+            <ApercuArticles dataArticles={dataNull} />
           </div>
         </div>
       </div>
