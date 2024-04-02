319138cd9934dd3f5c4c15492337815f7355cea6
reverse-smartass <107412957+reverse-smartass@users.noreply.github.com>
2024-04-02 16:17:18 -0400

    structure preliminaire page cart (statique a etre converti en dynamique)

--- website/eko-website/src/app/Cart/page.jsx
+++ website/eko-website/src/app/Cart/page.jsx
@@ -2,21 +2,44 @@
 import React, { useState, useEffect } from "react";
 import Header from "../Article/Header";
 import Footer from "../Article/Footer";
-export default function Cart(){
+export default function Cart() {
     return (
-    <div className="bg-[#F5F5F7]">
-    <Header />
-        <div className="min-h-screen">
-            
-            <h1>CART
+        <div className="bg-[#F5F5F7]">
+            <Header />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 border-160  mb-11 min-h-screen "
+                style={{
+                    borderRight: "160px solid transparent",
+                    borderLeft: "160px solid transparent",
+                }}>
 
-                COOKIE, ID DE SESSION
-  
-            </h1>
-            <p >content</p>
+                <div className="row-span-2 md:col-span-2 grid grid-cols-1 ">
+                    <div className="ml-40  grid h-20 w-auto place-items-left rounded-md border-2  bg-white py-2 text-justified ">
+                        <h1 className="mt-2 text-2xl font-bold tracking-tight text-black">
+                            Shopping Cart
+                        </h1>
+                    </div>
+                </div>
+
+                <div className="col-span-1 row-span-1 grid grid-cols-1 gap-4">
+                    <div className="bg-white p-4 rounded">
+                        <small class="m-6 text-gray-500 text-base font-black">
+                            Total
+                        </small>
+                        <div className="m-6 row-span-1 md:col-span-2 grid grid-cols-2 gap-4">
+                            <h2 className="text-xl font-bold">
+                                Sub-total
+                            </h2>
+
+                            <h2 className="text-xl font-semibold text-blue-800">
+                                220 $
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
-    <Footer />
-    </div>
-     );
+    );
 
 }
+
